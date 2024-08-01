5c458b3f2f2c903f048fe6ecd1674d5f35a09db6
kmu-jungsy <jshjsy01@kookmin.ac.kr>
2024-08-01 14:05:50 -0700

    modify code to run test.py

--- cuda_supported_gear/modeling_llamagear.py
+++ cuda_supported_gear/modeling_llamagear.py
@@ -201,7 +201,7 @@ class LlamaAttention_GEAR(nn.Module):
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
             kv_seq_len += past_key_value[8]
-        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        cos, sin = self.rotary_emb(value_states, position_ids)
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
         assert self.num_key_value_groups == 1
         # [bsz, nh, t, hd]
@@ -579,7 +579,6 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
 
         self.gradient_checkpointing = False
         self.compress_config = compress_config
-        # Initialize weights and apply final processing
         self.post_init()
 
     def get_input_embeddings(self):
@@ -609,7 +608,6 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        # retrieve input_ids and inputs_embeds
         if input_ids is not None and inputs_embeds is not None:
             raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
         elif input_ids is not None:
@@ -620,7 +618,7 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
             raise ValueError("You have to specify either input_ids or inputs_embeds")
 
         past_key_values_length = 0
-        if past_key_values is not None:
+        if past_key_values is not None and len(past_key_values) > 0:
             past_key_values_length = past_key_values[0][8]
 
         if position_ids is None:
@@ -634,15 +632,12 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
             inputs_embeds = self.embed_tokens(input_ids)
 
         if getattr(self.config, "_flash_attn_2_enabled", False):
-            # 2d mask is passed through the layers
             attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
         else:
-            # 4d mask is passed through the layers
             attention_mask = _prepare_4d_causal_attention_mask(
                 attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
             )
 
-        # embed positions
         hidden_states = inputs_embeds
 
         if self.gradient_checkpointing and self.training:
@@ -652,7 +647,6 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
                 )
                 use_cache = False
 
-        # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
         next_decoder_cache = () if use_cache else None
@@ -661,7 +655,7 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
 
-            past_key_value = past_key_values[idx] if past_key_values is not None else None
+            past_key_value = past_key_values[idx] if past_key_values is not None and len(past_key_values) > 0 else None
 
             if self.gradient_checkpointing and self.training:
                 layer_outputs = self._gradient_checkpointing_func(
@@ -693,7 +687,6 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
 
         hidden_states = self.norm(hidden_states)
 
-        # add hidden states from the last decoder layer
         if output_hidden_states:
             all_hidden_states += (hidden_states,)
 
@@ -706,6 +699,7 @@ class LlamaModel_GEAR(LlamaPreTrainedModel):
             hidden_states=all_hidden_states,
             attentions=all_self_attns,
         )
+
 
 
 class LlamaForCausalLM_GEARKIVI(LlamaPreTrainedModel):
@@ -877,3 +871,145 @@ class LlamaForCausalLM_GEARKIVI(LlamaPreTrainedModel):
                 tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
             )
         return reordered_past
+    
+
+class LlamaForCausalLM_GEARKIVI(LlamaPreTrainedModel):
+    _tied_weights_keys = ["lm_head.weight"]
+
+    def __init__(self, config, compress_config=None):
+        super().__init__(config)
+        self.model = LlamaModel_GEAR(config, compress_config)
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.compress_config = compress_config
+        self.post_init()
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def set_input_embeddings(self, value):
+        self.model.embed_tokens = value
+
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head = new_embeddings
+
+    def set_decoder(self, decoder):
+        self.model = decoder
+
+    def get_decoder(self):
+        return self.model
+
+    @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+    @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        """
+        This is the forward method for LlamaForCausalLM_GEARKIVI.
+
+        Returns:
+        """
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        loss = None
+        if labels is not None:
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+    def prepare_inputs_for_generation(
+        self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
+    ):
+        if past_key_values is not None and len(past_key_values) > 0:
+            past_length = past_key_values[0][8]
+            if input_ids.shape[1] > past_length:
+                remove_prefix_length = past_length
+            else:
+                remove_prefix_length = input_ids.shape[1] - 1
+            input_ids = input_ids[:, remove_prefix_length:]
+
+        position_ids = kwargs.get("position_ids", None)
+        if attention_mask is not None and position_ids is None:
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past_key_values:
+                position_ids = position_ids[:, -input_ids.shape[1] :]
+
+        if inputs_embeds is not None and (past_key_values is None or len(past_key_values) == 0):
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        else:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "position_ids": position_ids,
+                "past_key_values": past_key_values,
+                "use_cache": kwargs.get("use_cache"),
+                "attention_mask": attention_mask,
+            }
+        )
+        return model_inputs
+
+    @staticmethod
+    def _reorder_cache(past_key_values, beam_idx):
+        reordered_past = ()
+        for layer_past in past_key_values:
+            reordered_past += (
+                tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
+            )
+        return reordered_past
+
--- cuda_supported_gear/test.py
+++ cuda_supported_gear/test.py
@@ -1,111 +1,109 @@
-#
-from modeling_llamagear import LlamaForCausalLM_GEARKIVI
-from modeling_llama_kivi import LlamaForCausalLM_KIVI
+import os
+import gc
+import torch
 from transformers import LlamaConfig, AutoTokenizer, LlamaForCausalLM
 from transformers import BitsAndBytesConfig
 from datasets import load_dataset
-import torch
 import argparse
+from modeling_llamagear import LlamaForCausalLM_GEARKIVI
+from modeling_llama_kivi import LlamaForCausalLM_KIVI
 
-hf_token = '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'
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
 
-#### Config for KIVI model
-config = LlamaConfig.from_pretrained("meta-llama/Llama-2-7b-hf", token=hf_token)
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
-config.k_bits = 2# current support 2/4 bit for KV Cache
-config.v_bits = 2 # current support 2/4 bit for KV Cache
+config = LlamaConfig.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+config.k_bits = 2  # current support 2/4 bit for KV Cache
+config.v_bits = 2  # current support 2/4 bit for KV Cache
 config.group_size = 64
-config.residual_length = 64 # the number of recent fp16 tokens
+config.residual_length = 64  # the number of recent fp16 tokens
 
-# quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 parser = argparse.ArgumentParser(description="Evaluate AQuA Tasks")
 parser.add_argument("--batch_size", type=int, default=8, help="Batch size.")
 parser.add_argument("--model", type=str, default="meta-llama/Llama-2-7b", help="Model name or path.")
 args = parser.parse_args()
 
-max_token = 1000 ### prefill_length
-max_generation_length = 1500 ### geneate 500
+max_token = 1000  ### prefill_length
+max_generation_length = 1500  ### geneate 500
 batch_size = args.batch_size
 
 ##### Config for 
 compress_config = {}
-compress_config["compress_method"] = "gearlKIVI" # "gearlKIVI" "gearsKIVI"
+compress_config["compress_method"] = "gearlKIVI"  # "gearlKIVI" "gearsKIVI"
 compress_config["group_size"] = 64
 compress_config["residual"] = 64
 compress_config["quantize_bit"] = 2
-compress_config["rank"] = 2 ## prefill rank
-compress_config["rankv"] = 2 ## prefill rank
+compress_config["rank"] = 2  ## prefill rank
+compress_config["rankv"] = 2  ## prefill rank
 compress_config["loop"] = 3
-# compress_config["stream_list"] = stream_list
-stream_list = [torch.cuda.Stream(),torch.cuda.Stream()]
 
-args.model = "None"
+args.model = "gearl"
+
 if "gearl" in args.model:
     model = LlamaForCausalLM_GEARKIVI.from_pretrained(
         "meta-llama/Llama-2-7b-hf",
-        config = config,
-        # quantization_config = quantization_config,
-        compress_config = compress_config,
-        device_map = "cuda:0",
-        token=hf_token
+        config=config,
+        quantization_config=quantization_config,
+        compress_config=compress_config,
+        device_map="cuda:0"
     )
 elif "KIVI" in args.model:
     model = LlamaForCausalLM_KIVI.from_pretrained(
         "meta-llama/Llama-2-7b-hf",
-        config = config,
-        # quantization_config = quantization_config,
-        # compress_config = compress_config,
-        
-        device_map = "cuda:0",
-        token=hf_token
+        config=config,
+        quantization_config=quantization_config,
+        device_map="cuda:0"
     )
 elif "None" in args.model:
     model = LlamaForCausalLM.from_pretrained(
-    "meta-llama/Llama-2-7b-hf",
+        "meta-llama/Llama-2-7b-hf",
+        quantization_config=quantization_config,
+        device_map="cuda:0"
+    )
 
-    device_map = "cuda:0",
-    token=hf_token)
-
-model = model.half()
-
-
-
-
+# model = model.half() # 이 줄을 제거합니다.
 
 tokenizer = AutoTokenizer.from_pretrained(
-    'meta-llama/Llama-2-7b-hf', 
+    'meta-llama/Llama-2-7b-hf',
     model_max_length=max_token,
     max_length=max_token,
-    use_fast=False, 
+    use_fast=False,
     trust_remote_code=True,
-    use_auth_token=hf_token)
+    tokenizer_type='llama'
+)
 tokenizer.pad_token = tokenizer.eos_token
 test = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
 text_combined = test["text"]
 
 sentence_group = []
 for i in range(batch_size):
-    # sentence_group.append(str(text_combined[i*max_token:(i+1)*max_token]))
     sentence_group.append(str(text_combined[0:max_token]))
+
 inputs = tokenizer(
     sentence_group,
     return_tensors="pt",
     padding="max_length",
     truncation=True,
 )
+
 print("begin")
 inputs = inputs.to("cuda:0")
 print(inputs.input_ids.shape)
+
+gc.collect()
+torch.cuda.empty_cache()
+
 import time
 
 start = time.time()
-result = model.generate(**inputs, max_length=max_generation_length, use_cache=True)
+result = model.generate(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'], max_length=max_generation_length, use_cache=True)
 torch.cuda.synchronize()
 end = time.time()
-peak_memory = torch.cuda.max_memory_allocated(device="cuda") / (1024**2)  # 转换为MB单位
 
+peak_memory = torch.cuda.max_memory_allocated(device="cuda") / (1024**2)  # MB 단위로 변환
 print(f"Peak memory usage on GPU: {peak_memory} MB")
-print("time",end - start)
-# result = tokenizer.batch_decode(result, skip_special_tokens=True)
-# print(result)
-# model = model.cuda()
+print("time", end - start)
+
+gc.collect()
+torch.cuda.empty_cache()
